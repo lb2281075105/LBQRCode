231752ac115af66a658a1b2f7a16b9b0f29b16ee
lb2281075105 <2281075105@qq.com>
2018-11-14 11:29:05 +0800

    初始化扫描二维码项目

---
+++ LBQRCode/LBQRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LBQRCode
+//
+//  Created by liubo on 2018/11/14.
+//  Copyright © 2018年 JCSON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LBQRCode/LBQRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LBQRCode
+//
+//  Created by liubo on 2018/11/14.
+//  Copyright © 2018年 JCSON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
