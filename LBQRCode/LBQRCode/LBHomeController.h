90c14a6f2afd6501a830b08297e0920ddc480d71
lb2281075105 <2281075105@qq.com>
2018-11-14 11:58:49 +0800

    添加pod

--- LBQRCode/LBQRCode/LBHomeController.h
+++ LBQRCode/LBQRCode/LBHomeController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  LBHomeController.h
 //  LBQRCode
 //
 //  Created by liubo on 2018/11/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface LBHomeController : UIViewController
 
 @end
-
---
+++ LBQRCode/LBQRCode/LBHomeController.m
@@ -0,0 +1,31 @@
+//
+//  LBHomeController.m
+//  LBQRCode
+//
+//  Created by liubo on 2018/11/14.
+//  Copyright © 2018年 JCSON. All rights reserved.
+//
+
+#import "LBHomeController.h"
+
+@interface LBHomeController ()
+
+@end
+
+@implementation LBHomeController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+    self.title = @"二维码、条形码";
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+    
+    
+    
+}
+
+
+@end
